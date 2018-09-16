b6be142a8e2683eaccd45483d5274d46fbb8e3ab
Kalyani Asthana <kalyaniasthana@gmail.com>
2018-09-16 16:52:31 +0530

    recursive function for number to pattern conversion

--- ch6.py
+++ ch6.py
@@ -8,6 +8,15 @@ def PatternToNumber(pattern):
 		p -= 1
 	return num
 
+def SymbolToNumber(symbol):
+	symbol = symbol.lower()
+	d = {'a' : 0, 't': 3, 'g': 2, 'c': 1}
+	return d[symbol]
+
+def NumberToSymbol(number):
+	d = {0: 'a', 1: 'c', 2: 'g', 3: 't'}
+	return d[number].upper();
+
 def NumberToPattern(number, k):
 	pattern = ""
 	d = {0: 'a', 1: 'c', 2: 'g', 3: 't'}
@@ -27,6 +36,35 @@ def ComputingFrequencies(text, k):
 		j = PatternToNumber(pattern)
 		FrequencyArray[j] += 1
 
-	return ' '.join(str(i) for i in FrequencyArray)
+	return FrequencyArray
 
-print ComputingFrequencies('CCTCTACCTGGCTCCGTGACCTGCCGGTGACGTTATATCTTGCATTCGCCGAACTCTAATTCGGGCCTTGCGCCTTTAGGAATTGCAATGTCTTTCATGACTCAACCTCCTAGTGATTCCAGTAACTCTTTTGCTTTCTTGAGCAGAGTAGCTTAGATCCGCTTTAACCTAACTCAGAGCGCCAATTAGCCGATCATTGCTCCCACTGTGGGCAACGGCGCCGTGCTGCCTCTTCAAGCCGATTGATCGATACCCTGAGCAACATAATAACAGGTGACGGTCGTACCGGATTTTAAGATGACTGGAACTAGATGTCCCACTGCGTCCGGCAGCGATCCAACTCGCTCCGCGGAATTGACATTTCCTGCATAAACTGCCGCCCGATTAGGCAAAGCTTTGGCAAGGTGCCCTACGAACGACACTCTAATGCGCTGAGACTTGAGTGTAGCGGTTTTGCACACGAATTCGTTATGTCACGTCATTGCGAGTCGGACCCTCGACACACAAACAAACAGTTGTGCGTTTTGTCCCCACCGACAAGGCAAGCTAAACGGAAATGACGTTTACGAGGCGTGATTCTTTTCTCCCTGTGCTCAAGGGGTAGTTCA', 7)
+def FasterFrequentTerms(text, k):
+	FrequentPatterns = []
+	FrequencyArray = ComputingFrequencies(text, k)
+	maxCount = max(FrequencyArray)
+	for i in range(0, len(FrequencyArray)):
+		if FrequencyArray[i] == maxCount:
+			pattern = NumberToPattern(i, k)
+			FrequentPatterns.append(pattern)
+
+	return FrequentPatterns
+
+def RecursivePatternToNumber(pattern):
+
+	if pattern == "":
+		return 0
+	symbol = pattern[-1]
+	prefix = pattern[0: len(pattern) - 1]
+	return 4*RecursivePatternToNumber(prefix) + SymbolToNumber(symbol)
+
+def RecursiveNumberToPattern(number, k):
+	if k == 1:
+		return NumberToSymbol(number)
+	remainder = number%4
+	quotient = number/4
+	symbol = NumberToSymbol(remainder)
+	prefix = RecursiveNumberToPattern(quotient, k - 1)
+	return prefix + symbol
+
+print RecursivePatternToNumber('GGGTTCATAGCTCTT')
+print RecursiveNumberToPattern(8444, 7)
