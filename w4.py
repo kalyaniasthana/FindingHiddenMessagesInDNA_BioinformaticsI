24994222d3cb4fa63d8c40d3000c10ef279be30f
Kalyani Asthana <kalyaniasthana@gmail.com>
2018-10-09 18:03:18 +0530

    gibbs sampler not working- I'm doing something wrong

--- w4.py
+++ w4.py
@@ -3,6 +3,7 @@ from w2_2 import *
 from ch6 import *
 import numpy
 import random
+import numpy as np
 
 def inputFile(file):
     lines = file.readline().split()
@@ -55,14 +56,75 @@ def RandomizedMotifSearchWithIterations(dna, k, t):
 			break
 	return best_motifs
 
+def ProbabilityMatrix(motifs, profile):
+	probs = []
+	for motif in motifs:
+		prob = 1
+		for j in range(0, len(motif)):
+			if motif[j] == 'A':
+				prob *= profile['A'][j]
+			elif motif[j] == 'C':
+				prob *= profile['C'][j]
+			elif motif[j] == 'G':
+				prob *= profile['G'][j]
+			elif motif[j] == 'T':
+				prob *= profile['T'][j]
 
-file = inputFile(open('../Downloads/dataset_161_5 (1).txt'))
-dna = file[0]
-k = file[1]
-t = file[2]
-motifs = RandomizedMotifSearchWithIterations(dna, k, t)
-for motif in motifs:
-	print motif
+		probs.append(prob)
+	if sum(probs) != 1:
+		my_sum = sum(probs)
+		for prob in probs:
+			prob /= my_sum
+	return probs
 
+
+def ProfileRandomlyGeneratedKmer(probability_list, dnai, k):
+	list_of_candidate_kmers = []
+	for i in range(0, len(dnai) - k + 1):
+		list_of_candidate_kmers.append(dnai[i:i+k])
+	motif_i = list_of_candidate_kmers[np.argmin((np.cumsum(probability_list) / sum(probability_list)) < np.random.rand())]
+	return motif_i
+
+
+'''
+def RandomChoice(probs, dnai):
+	my_prob = random.choice(probs)
+	my_motif = ''
+	#for prob in probs:
+		#if prob == my_probs:
+	for i in range(0, len(motifs)):
+		if probs[i] == my_prob:
+			my_motif = motifs[i]
+			break
+	return my_motif'''
+
+def GibbsSampler(dna, k, t, N):
+	motifs = []
+	best_motifs = []
+	for dna_ in dna:
+			random_number = random.randint(0,len(dna_) - k)
+			kmer = dna_[random_number: random_number + k]
+			motifs.append(kmer)
+	best_motifs = motifs
+	best_score = 99999
+	for j in range(0, 20):
+		i = random.choice(range(t))
+		motifs.remove(motifs[i])
+		profile = ProfileMatrixFromMotifsWithPseudocounts(motifs, 1)
+		probability_list = ProbabilityMatrix(motifs, profile)
+		my_motif = ProfileRandomlyGeneratedKmer(probability_list, dna[i], k)
+		motifs.insert(i, my_motif)
+		my_score = score(motifs)
+		if my_score < best_score:
+			best_motifs = motifs
+			best_score = my_score
+
+
+	return best_motifs
+
+dna = 'CGCCCCTCTCGGGGGTGTTCAGTAACCGGCCA GGGCGAGGTATGTGTAAGTGCCAAGGTGCCAG TAGTACCGAGACCGAAAGAAGTATACAGGCGT TAGATCAAGTTTCAGGTGCACGTCGGTGAACC AATCCACCAGCTCCACGTGCAATGTTGGCCTA'
+dna = dna.split(' ')
+
+print GibbsSampler(dna, 8, 5, 100)
 
 
